fe8c86918b817b04470aa418b40c9250a8063fcb
Berente Péter <40863181+berentepeter@users.noreply.github.com>
2023-10-09 14:55:04 +0200

    Update pw_gen.sql

--- pw_gen.sql
+++ pw_gen.sql
@@ -3,10 +3,10 @@ Use the code below to generate password.
 Change @priv_key variable then execute line 6-9.
 @priv_key must be less than 2147483647
 ******************************************************************************/
-DECLARE @priv_key INT 	=    PRIVATE_KEY;
-DECLARE @pub_key  INT 	=    (SELECT ABS(CHECKSUM(NewId())));
-SELECT  @pub_key  			 [public key];
-SELECT  @priv_key & @pub_key [password];
+DECLARE @priv_key INT 	= PRIVATE_KEY;
+DECLARE @pub_key  INT 	= (SELECT ABS(CHECKSUM(NewId())));
+SELECT  @pub_key  AS 'public key';
+SELECT  @priv_key & @pub_key AS 'password';
 
 /******************************************************************************
 Use the code below to get password from publlic key.
@@ -14,5 +14,5 @@ Replace @pubkey value with the public key generated before.
 Change @privatekey value to your private key then execute line 16-18
 ******************************************************************************/
 DECLARE @pubkey     INT  	= PUBLIC_KEY;
-DECLARE @privatekey INT     = PRIVATE_KEY;
-SELECT  @pubkey & @privatekey [password];
+DECLARE @privatekey INT   = PRIVATE_KEY;
+SELECT  @pubkey & @privatekey AS 'password';
